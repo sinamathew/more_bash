68d2fc0d3499c1d2bec371bb2b230af0eea68f40
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-24 22:04:51 +0100

    Command-line arguments

---
+++ 1-bash_fundamentals/4-argument_analyser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Accepts two command-line arguments
+arg1="${1}"
+arg2="${2}"
+
+# Display the number of arguments
+echo "The total number of arguments taking is 2."
+
+# Display the values of arguments
+echo "The value of argument 1 is ${arg1}."
+echo "The value of argument 2 is ${arg2}."
