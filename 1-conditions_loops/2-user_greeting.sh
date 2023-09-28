ed58f0575910b9bf675f1d6e0e0f41cbebfe2be4
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-28 18:33:29 +0100

    Keep asking for name until you enter something

---
+++ 1-conditions_loops/2-user_greeting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ask for user's name
+read -p "What's your name? " name
+
+# Ask for name again if user does not input anything
+while [[ -z ${name} ]]
+do
+	read -p "Please enter your name " name
+done
+
+echo "Good day, ${name}! Trust you're doing great today."
