02b707a9051eb1ed56cf31ea78493b43c09eff08
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-29 20:10:36 +0100

    Added code indentation.

--- 1-conditions_loops/6-sum_calculation.sh
+++ 1-conditions_loops/6-sum_calculation.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Declare variable i and sum
 i=0
 sum=0
 
+# Loop to add all numbers from 1 to 10
 while (( i <= 10 )) do
-	sum=$((sum + i))
-	i=$((i + 1))
+	sum=$( (sum + i ))
+	i=$(( i + 1 ))
 done
 
+# Print out the sum of the numbers
 echo "The sum of 1 to 10 is: ${sum}."
