48f8166591c4a6a7548f155d6475a12405b29fad
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-30 18:38:53 +0100

    Script works, adding documentation and other features soon.

--- 1-conditions_loops/8-number_guessing.sh
+++ 1-conditions_loops/8-number_guessing.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-#
-#
-#
 
+target=$((RANDOM % 100 + 1))
 
+while true; do
+	read -p "Guess a lucky number: " guess
 
+	if [[ ${guess} -eq ${target} ]] then
+		echo "Yes, you guess right :)."
+		exit 0
+	elif [[ ${guess} -gt ${target} ]] then
+		echo "Your guess is high, guess again."
+	else
+		echo "Your guess is low, try again."
+	fi
+done
