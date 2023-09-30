f4adb931efc22df3f0faa650e4b6077b2be2a2c4
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-30 18:47:32 +0100

    Added valid & negative number checker feature.

--- 1-conditions_loops/8-number_guessing.sh
+++ 1-conditions_loops/8-number_guessing.sh
@@ -5,12 +5,17 @@ target=$((RANDOM % 100 + 1))
 while true; do
 	read -p "Guess a lucky number: " guess
 
-	if [[ ${guess} -eq ${target} ]] then
-		echo "Yes, you guess right :)."
-		exit 0
-	elif [[ ${guess} -gt ${target} ]] then
-		echo "Your guess is high, guess again."
+	if [[ ${guess} =~ ^-?[[:digit:]]+$ ]] then
+
+		if [[ ${guess} -eq ${target} ]] then
+			echo "Yes, you guess right :)."
+			exit 0
+		elif [[ ${guess} -gt ${target} ]] then
+			echo "Your guess is high, guess again."
+		else
+			echo "Your guess is low, try again."
+		fi
 	else
-		echo "Your guess is low, try again."
+		echo "What you entered is not a valid number."
 	fi
 done
