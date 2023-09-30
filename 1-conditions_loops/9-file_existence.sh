d63055f42cd2f127c64e0299c93a05327bda7874
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-30 18:16:47 +0100

    Added documentation and fixes. Yeah....I optimized the script :)

--- 1-conditions_loops/9-file_existence.sh
+++ 1-conditions_loops/9-file_existence.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
+# Prints a new line
 echo 
 
+# Check for argument
 if [[ ${#} -eq 0 ]] then
 	echo "Usage: ${0} filename1 filename2 ..."
 	exit 1
 fi
 
+# Loop to check all files in the argument
 for file in "${@}"; do
+
+	# Check for file existence
 	if [[ -e ${file} ]] then
 		echo -n "The file '${file}' exists and it's "
 
-		if [[ -s ${file} ]] then
-			echo -n "not empty"
-		fi
-
+		# Check for file read permission
 		if [[ -r ${file} ]] then
-			echo -n ", readable"
+			echo -n "readable"
+		else
+			echo -n "not readable"
 		fi
 
+		# Check for file write permission
 		if [[ -w ${file} ]] then
 			echo -n ", writable"
+		else
+			echo -n ", not writable"
 		fi
 
+		# Check for file execute permission
 		if [[ -x ${file} ]] then
 			echo -n ", executable"
+		else
+			echo -n ", not executable"
 		fi
 
-		if [[ ! -s ${file} && ! -r ${file} && ! -w ${file} && ! -x ${file} ]] then
-			echo -n "empty with no permission"
+		# Check for file size
+		if [[ -s ${file} ]] then
+			echo -n " and not empty"
+		else
+			echo -n " and empty"
 		fi
 		
+		# Add period and new line
 		echo "."
+
+	# If file doesn't exist
 	else
 		echo "The file '${file} doesn't exist."
 	fi
 
+	# Prints new line
 	echo
 done
